ab19153a48c837704071ac97496ebee2bcceaa5b
Spartaco Amadei <spartaco.amadei@outlook.it>
2026-06-28 06:02:51 +0200

    docs: add doc and help to functions

--- Public/Get-WslVirtualDrivePaths.ps1
+++ Public/Get-WslVirtualDrivePaths.ps1
@@ -1,3 +1,12 @@
+<#
+.SYNOPSIS
+    Gets the paths of virtual drives used by WSL.
+.DESCRIPTION
+    This function retrieves the paths of virtual drives used by Windows Subsystem for Linux (WSL). It searches for .vhdx files in the Docker and Windows Packages directories within the user's local application data folder.
+.EXAMPLE
+    # Get the paths of virtual drives used by WSL
+    Get-WslVirtualDrivePaths
+#>
 function Get-WslVirtualDrivePaths {
     $DockerFiles = Get-ChildItem $env:LOCALAPPDATA\Docker\*\*.vhdx -Recurse
     $SystemFiles = Get-ChildItem $env:LOCALAPPDATA\Packages\*\*.vhdx -Recurse
--- Public/Start-WslVirtualDriveCleanup.ps1
+++ Public/Start-WslVirtualDriveCleanup.ps1
@@ -1,4 +1,15 @@
-# Import needed functions
+<#
+.SYNOPSIS
+    Cleans up virtual drives used by WSL.
+.DESCRIPTION
+    This function shuts down all WSL instances and optimizes the virtual drives used by Windows Subsystem for Linux (WSL). It retrieves the paths of .vhdx files in the Docker and Windows Packages directories within the user's local application data folder and optimizes them using the Optimize-VHD cmdlet.
+.EXAMPLE
+    # Clean up virtual drives used by WSL
+    Start-WslVirtualDriveCleanup
+.NOTES
+    Requires WSL to be installed and the Optimize-VHD cmdlet to be available (part of the Hyper-V module). 
+    Ensure you have the necessary permissions to perform these operations.
+#>
 function Start-WslVirtualDriveCleanup {
     
     Write-Host "[*] Shutting down wsl machines..." -ForegroundColor Blue
